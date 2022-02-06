b8a6ea9788970bfe98c3cc2276e7e3968d7c3f29
Dhritiman-Das <iamdhritiman01@gmail.com>
2022-02-06 17:07:58 +0530

    Changes made

---
+++ scripts/generateStories.js
@@ -0,0 +1,32 @@
+// function generateStories() {
+//     for div in document.getElementsByClassName('divCheckpoint') {
+
+//     }
+// }
+
+// if checkpointTop!='none':
+//     use
+//         '-checkpoint : ${checkpointTop.id}
+//         '
+// if multiple intents:
+//     use
+//         '-or:
+//             - intent:
+//             - intent:
+//             '
+// else if single intent:
+//     use
+//         '-intent'
+// getActionNames()
+
+//rename generateStories to getActionNames
+// function generateStories() {
+// var actionString = ""
+//   var dummyDiv = document.getElementsByClassName("divCheckpoint")[0];
+//   for (const actionsDiv of dummyDiv.children[1].children[1].children) {
+//     console.log(actionsDiv.classList.contains("utteranceBlock"));
+//     if (actionsDiv.classList.contains("utteranceBlock")) {
+
+//     }
+//   }
+// }
